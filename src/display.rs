84742eef892dee920b9f11ebe7f0c06e6600547f
Justin Dubs <jtdubs@gmail.com>
2020-03-23 09:37:49 -0400

    start of refactor

--- src/display.rs
+++ src/display.rs
@@ -9,6 +9,13 @@ pub enum State {
 }
 
 #[derive(Debug)]
+#[allow(dead_code)]
+pub enum RegisterSelector {
+    Instruction = 0,
+    Data        = 1
+}
+
+#[derive(Debug)]
 pub struct HD44780U {
     state : State,
     addr  : u8
@@ -20,6 +27,20 @@ impl HD44780U {
             state: State::Busy(15000),
             addr:  0
         }
+    }
+
+    pub fn read(&self, addr: RegisterSelector) -> u8 {
+        let mut data = self.addr;
+        if let State::Busy(_) = self.state {
+            data |= 0x80;
+        }
+        debug!("R {:?} = {:02x}", addr, data);
+        data
+    }
+
+    pub fn write(&mut self, addr: RegisterSelector, val: u8) {
+        debug!("W {:?} = {:02x}", addr, val);
+        unimplemented!();
     }
 }
 
@@ -36,16 +57,10 @@ impl periph::PortAttachment for HD44780U {
     }
 
     fn read(&self) -> u8 {
-        let mut data = self.addr;
-        if let State::Busy(_) = self.state {
-            data |= 0x80;
-        }
-        debug!("R {:02x}", data);
-        data
+        unimplemented!();
     }
 
-    fn write(&self, val : u8) {
-        debug!("W {:02x}", val);
+    fn write(&self, _val : u8) {
         unimplemented!();
     }
 }
